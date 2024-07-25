9b6b2dae2dbbde5b2a7153a3a6fce5b53d023070
Aaby Abhay <aaby242abhay@gmail.com>
2024-07-25 13:30:27 +0530

    adding atoic transactions

--- backend/routes/accounts.js
+++ backend/routes/accounts.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const {Account} = require("../db");
+const { authMiddleware } = require("../middleware");
+const mongoose = require("mongoose");
+
 
 const accountRouter = express.Router();
 
@@ -16,12 +19,12 @@ accountRouter.post('/transfer', authMiddleware, async (req,res) =>{
     
     // const {to, amount} = req.body;
 
-    // const account = Account.findOne({userId : req.userId});
+    // const account = await Account.findOne({userId : req.userId});
     // if(account.balance < amount){
     //     return res.status(400).send({error : "Insufficient Balance"});
     // }
 
-    // const toAccount = Account.findOne({userId : to});
+    // const toAccount = await Account.findOne({userId : to});
     // if(!toAccount){
     //     return res.status(400).send({error : "Invalid Account"});
     // }
@@ -41,6 +44,40 @@ accountRouter.post('/transfer', authMiddleware, async (req,res) =>{
     // })
 
     /**********     GOOD SOLUTION     ******************/
+    
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    const {to, amount} = req.body;
+    console.log(to);
+    const account = await Account.findOne({userId : req.userId}).session(session);
+    if(!account || account.balance < amount){
+        await session.abortTransaction();
+        return res.status(400).send({error : "Insufficient Balance"});
+    }
+    const toAccount = await Account.findOne({userId : to}).session(session);
+    if(!toAccount){
+        await session.abortTransaction();
+        return res.status(400).send({
+            message : "Invalid Account"
+        })
+    }
+    await Account.updateOne({userId : req.userId},{
+        $inc : {
+            balance : -amount,
+        }
+    }).session(session);
+
+    await Account.updateOne({userId : to}, {
+        $inc : {
+            balance : amount
+        }
+    }).session(session);
+    await session.commitTransaction();
+    return res.status(200).json({
+        message : "Transaction successful"
+    })
+
 })
 
 module.exports = accountRouter;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const userRouter = require("./user.js");
-const accountRouter = require("./account.js");
+const accountRouter = require("./accounts.js");
 
 const router = express.Router();
 router.use('/user',userRouter);

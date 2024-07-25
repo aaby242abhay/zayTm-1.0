f3907747cd2f0e07c90bf2148135f2527dc8a2b9
Aaby Abhay <aaby242abhay@gmail.com>
2024-07-25 15:43:43 +0530

    routing routes and initialising dependencies

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,24 @@
+import React from "react"
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
+import { Signin } from "./components/Signin"
+import { Signup } from "./components/Signup"
+import { SendMoney } from "./components/SendMoney"
+import { Dashboard } from "./components/Dashboard"
 
 function App() {
 
   return (
-    <div>
-        Hello world
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/sendmoney" element={<SendMoney />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/" element={<Navigate to="/signin" />} />
+        </Routes>
+      </Router>
+    </>
   )
 }
 

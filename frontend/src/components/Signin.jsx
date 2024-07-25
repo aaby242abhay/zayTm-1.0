9c50bcb0a0b99027f7d02482d9793dc47c24af8a
Aaby Abhay <aaby242abhay@gmail.com>
2024-07-26 02:16:35 +0530

    comp

--- frontend/src/components/Signin.jsx
+++ frontend/src/components/Signin.jsx
@@ -5,7 +5,7 @@ export const Signin = () => {
     <div className=' shadow-2xl flex justify-center mt-20 mx-auto max-w-md ' >
       <div className='-p-10'>
         <div className='text-3xl font-bold pb-2 pl-24 mt-4'>
-          Sign Up</div>
+          Sign In</div>
         <div className='text-gray-500 font-semibold p-2 text-center'>Enter the credentials to access your <br/>account.</div>
         <div className='font-semibold m-1'>Email</div>
         <input className ="box-border h-5 w-full p-4 border-2 m-1 rounded-md" type ="text" placeholder='johndoe@example.com'></input>
